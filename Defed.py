abd5e0b1fba29a5bd2bd812b8f801a7c94346f38
Eric Dannewitz <ericdannewitz@me.com>
2022-12-25 11:27:43 -0800

    Updates

--- Defed.py
+++ Defed.py
@@ -1,11 +1,12 @@
-# Masto Defederation tool by Clarjon1!
-# Public domain code, remix and do as ye will. Please be kind!
-
-from mastodon import Mastodon
+from mastodon import Mastodon, MastodonError
 import pandas as pd
 import os, sys
 
 '''
+
+# Original -> Masto Defederation tool by Clarjon1!
+# Public domain code, remix and do as ye will. Please be kind!
+
 From your command line, 
 
 python Defed.py csvfilename.csv
@@ -16,6 +17,8 @@ Example would be
 domain,severity,private_comment,public_comment
 pieville.net,suspend,taken from a.rathersafe.space,"anti semitism, national socialism, racism, ableism, HomoMisia"
 
+This will currently update statuses if they are in your instance, so you can add public/private stuff or update if it is systemed or slienced or noop, OR add them if they are not in there.
+
 Enjoy!
 '''
 
@@ -25,7 +28,7 @@ def LoadCSV(defederation_csv_filename):
 
 def ProcessDomains(BlockList):
     ## Access Token
-    mytoken = "tokengoeshere"
+    mytoken = "TOKEN"
     ### Instance URL
     myinstance = "mydomain.social"
     '''
@@ -44,14 +47,33 @@ def ProcessDomains(BlockList):
   },
     '''
     m = Mastodon(access_token=mytoken, api_base_url=myinstance)
-    print(m)
+    blocks1 = m.admin_domain_blocks()
+    listofallblocks = m.fetch_remaining(blocks1)
+    listof = pd.DataFrame(listofallblocks)
+    # convert to Pandas cuz Pandas is kwel
     for i in BlockList.index:
-        print('Blocking Domain->' + str(BlockList['domain'][i]) + ' Severity->' + str(BlockList['severity'][i]) + ' Private_Comment->' + str(BlockList['private_comment'][i]) + ' Public_comment->' + str(BlockList['public_comment'][i]))
-        m.admin_create_domain_block(domain=str(BlockList['domain'][i]), 
-                                    severity=BlockList['severity'][i],
-                                    private_comment=BlockList['private_comment'][i],
-                                    public_comment=BlockList['public_comment'][i])
- 
+        # first see if the domain is already in there, and if so, update it
+        panda_row = listof[listof['domain'] == BlockList['domain'][i]].index.to_numpy()
+        if panda_row.size > 0:
+            print('Updating Status Domain->' + str(BlockList['domain'][i]) + ' Severity->' + str(BlockList['severity'][i]) + ' Private_Comment->' + str(BlockList['private_comment'][i]) + ' Public_comment->' + str(BlockList['public_comment'][i]))
+            try:
+                m.admin_update_domain_block(id=int(listof.iloc[panda_row]['id']),
+                                            severity=BlockList['severity'][i],
+                                            private_comment=BlockList['private_comment'][i],
+                                            public_comment=BlockList['public_comment'][i])
+            except MastodonError as e:
+                print(e)
+        else:
+            #ok not in instance, so lets add it
+            print('Adding Domain->' + str(BlockList['domain'][i]) + ' Severity->' + str(BlockList['severity'][i]) + ' Private_Comment->' + str(BlockList['private_comment'][i]) + ' Public_comment->' + str(BlockList['public_comment'][i]))
+            try:
+                m.admin_create_domain_block(domain=str(BlockList['domain'][i]), 
+                                            severity=BlockList['severity'][i],
+                                            private_comment=BlockList['private_comment'][i],
+                                            public_comment=BlockList['public_comment'][i])
+            except MastodonError as e:
+                print(e)
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Need a csv filename')
